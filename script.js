63f4b0ce657679603e95784fe9a76e6659fe149b
Nitu Dubey <108494260+Nitu170287@users.noreply.github.com>
2023-06-21 09:55:40 -0400

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+const submitBtn = document.getElementById("submit")
+const inputCheck = document.getElementById("checkbox")
+const inputUser = document.getElementById("username")
+const inputPass =  document.getElementById("password")
+const existBtn = document.getElementById("existing")
+existBtn.style.display="none"
+
+
+submitBtn.addEventListener("click",(event)=>{
+	
+	if(inputCheck.checked){
+		localStorage.setItem("username",inputUser.value)
+		localStorage.setItem("password",inputPass.value)
+		existBtn.style.display="inline"
+	}
+	alert("Logged in as "+ inputUser.value)
+})
